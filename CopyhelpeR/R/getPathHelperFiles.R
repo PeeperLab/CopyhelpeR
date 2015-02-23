07439cab72eccdfc3a8f74c3184296cc7b5ac4e2
Thomas Kuilman <t.kuilman@nki.nl>
2015-02-23 23:27:47 +0100

    Combined helper files into single file

--- CopyhelpeR/R/getPathHelperFiles.R
+++ CopyhelpeR/R/getPathHelperFiles.R
@@ -3,7 +3,7 @@ getPathHelperFiles <- function(reference.genome) {
            "hg19" = dir(system.file(package = "CopyhelpeR", "extdata"),
                         pattern = "^hg19", full.names = TRUE),
            "mm9" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                        pattern = "^mm9", full.names = TRUE),
+                       pattern = "^mm9", full.names = TRUE),
            "mm10" = dir(system.file(package = "CopyhelpeR", "extdata"),
                         pattern = "^mm10", full.names = TRUE),
            stop("There are currently no helper files available for reference ",
