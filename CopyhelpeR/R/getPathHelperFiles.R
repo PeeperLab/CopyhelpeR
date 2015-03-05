2ed93bd18141e2e4007f0d7b6524502ed5005d52
Thomas Kuilman <t.kuilman@nki.nl>
2015-03-05 14:57:51 +0100

    Bioconductor submission II

--- CopyhelpeR/R/getPathHelperFiles.R
+++ CopyhelpeR/R/getPathHelperFiles.R
@@ -1,11 +1,11 @@
-getPathHelperFiles <- function(reference.genome) {
-    switch(reference.genome,
+getPathHelperFiles <- function(ref.genome) {
+    switch(ref.genome,
            "hg19" = dir(system.file(package = "CopyhelpeR", "extdata"),
                         pattern = "^hg19", full.names = TRUE),
            "mm9" = dir(system.file(package = "CopyhelpeR", "extdata"),
                        pattern = "^mm9", full.names = TRUE),
            "mm10" = dir(system.file(package = "CopyhelpeR", "extdata"),
                         pattern = "^mm10", full.names = TRUE),
-           stop("There are currently no helper files available for reference ",
-                "genome ", reference.genome))
+           stop(.wrap("There are currently no helper files available for",
+                      "reference genome", sQuote(ref.genome))))
 }
